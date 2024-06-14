6b68f1b6e3cd742bddf4e94b547855added224ad
Dao_you <qaz00639@gmail.com>
2024-06-14 22:46:38 +0800

    Add expense/income feature

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ LOG_FILE = 'log.json'
 def init_log():
     if not os.path.isfile(LOG_FILE) or os.stat(LOG_FILE).st_size == 0:
         with open(LOG_FILE, 'w') as f:
-            data = {"sum": 0, "log": []}
+            data = {"income": 0, "expense": 0, "log": []}
             json.dump(data, f)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -20,7 +20,8 @@ def home():
     if request.method == 'POST':
         
         user_input = request.form['content_of_textbox'].strip()
-        category   = request.form['category']
+        category = request.form['category']
+        transaction_type = request.form['transaction_type']
 
         with open(LOG_FILE, 'r') as f:
             data = json.load(f)
@@ -31,11 +32,15 @@ def home():
             amount = int(amount)
             desc = str(desc).strip()
             dt = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            result = {"amount": amount, "description": desc, "datetime": dt, 'category': category}
+            result = {"amount": amount, "description": desc, "datetime": dt, 'category': category, 'type': transaction_type}
+
             with open(LOG_FILE, 'r') as f:
                 data = json.load(f)
             data['log'].insert(0, result)
-            data['sum'] += amount
+            if transaction_type == '收入':
+                data['income'] += amount
+            else:
+                data['expense'] += amount
             with open(LOG_FILE, 'w') as f:
                 json.dump(data, f)
 
@@ -46,14 +51,20 @@ def home():
                 init_log()
                 return render_template('result.html', info="Data Have Been Reset.")
             elif user_input == '/delete':
-                data['sum'] -= data['log'][0]['amount']
+                latest_entry = data['log'][0]
+                if latest_entry['type'] == '收入':
+                    data['income'] -= latest_entry['amount']
+                else:
+                    data['expense'] -= latest_entry['amount']
                 del data['log'][0]
                 with open(LOG_FILE, 'w') as f:
                     json.dump(data, f)
                 return render_template('result.html', info="Deleted Latest Data.")
             else:
                 return render_template('result.html', info="Error: Invalid input format.")
-    return render_template('home.html')
+    else:
+        return render_template('home.html')
+
 
 @app.route('/get_log', methods=['GET'])
 def get_log():
