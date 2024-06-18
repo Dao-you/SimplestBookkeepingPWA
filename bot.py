76ab478b332667c100643d62666e2f912c9b5e36
Dao_you <qaz00639@gmail.com>
2024-06-19 04:40:21 +0800

    Dsisable debug mode

--- bot.py
+++ bot.py
@@ -247,4 +247,4 @@ if __name__ == '__main__':
         app.run(host='0.0.0.0', debug=False, port=5000, ssl_context=SSH_KEY)
     else:
         print("Failed to load SSH keys, run it on HTTP without some PWA eatures!")
-        app.run(host='0.0.0.0', debug=True, port=5000)
+        app.run(host='0.0.0.0', debug=False, port=5000)
